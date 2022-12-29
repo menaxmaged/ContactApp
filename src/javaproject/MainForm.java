3548c1da16a0d9325d3c89af28e7da18ebf2167e
menaxmaged <menamaged150@gmail.com>
2022-12-29 02:06:05 +0200

    .......

--- src/javaproject/MainForm.java
+++ src/javaproject/MainForm.java
@@ -4,10 +4,8 @@
  */
 package javaproject;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
